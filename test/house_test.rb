2c9d2e8089d75f6f1b025ee37323e2fb4258b8e7
Mary Goodhart <marya.grace5@gmail.com>
2018-10-17 09:39:03 -0600

    The house has rooms and tests

--- test/house_test.rb
+++ test/house_test.rb
@@ -21,6 +21,21 @@ class HouseTest < Minitest::Test
     assert_equal "123 sugar lane", house.address
   end
 
+  def test_it_has_an_empty_array_for_rooms
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal [], house.rooms
+  end
+
+  def test_it_can_add_rooms
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, 13)
+    house.add_room(room_1)
+    assert_equal 1, house.rooms.count
+    room_2 = Room.new(:bedroom, 11, 15)
+    house.add_room(room_2)
+    assert_equal 2, house.rooms.count
+  end
+
 
 
 end
